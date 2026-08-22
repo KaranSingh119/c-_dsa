b0aef4795c1ff4525164b7c879fc6bbf465a253e
KaranSingh119 <kitthasingh@gmail.com>
2026-08-22 11:48:07 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (80.77%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int total=0 ;
+        int n= nums.size();
+        int sp=1 ;
+        
+        if (n<1){
+            return 0;
+        }
+        int unq=0 ;    
+        for (int i=1 ; i<n ;i++){
+            if (nums[i]!=nums[unq]){
+                unq++ ;
+                nums[unq]=nums[i];
+                
+            }
+        }
+        return unq+1;
+    }
+};
