d264e2746a5a209155382a3dd47cf9cdbd5c05d7
KaranSingh119 <kitthasingh@gmail.com>
2026-08-27 10:12:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (73.49%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isvalied(vector<int> &nums ,int k,int m,int maxpage){
+        int stu=1 ,page=0 ;
+        for (int i =0 ;i<k ; i++){
+            if (nums[i]> maxpage){
+                return false ;
+            }
+            if (page +nums[i] <= maxpage){
+                page +=nums[i];
+            }else{
+                stu++ ;
+                page= nums[i] ;
+            }
+        }
+         return stu > m ? false : true ;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int sum=0;
+        for (int i : nums){
+            sum+=i ;
+        }
+        int ans=-1;
+        int start=0 ,end=sum ;
+
+        while (start <= end){
+            int mid= start +(end-start)/2 ;
+
+            if (isvalied(nums,nums.size(),k, mid)){
+                ans=mid ;
+                end= mid-1;
+            }else{
+                start =mid+1 ;
+            }
+        }
+        return ans ;
+    }
+
+
+};
